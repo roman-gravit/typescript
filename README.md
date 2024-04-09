cab01d2f1bc70dbec6641bb4c86d20372439764d
Roman Gravit <romang@siber.com>
2024-04-09 11:49:18 +0100

    Enum

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-..
+## Typescript
+
+
+### Basic types is TypeScript
+
+   - any:     These may occur for values from code that has been written without TypeScript or a 3rd party library.
+              Unlike unknown, variables of type any allow you to access arbitrary properties, even ones that don’t exist. 
+              These properties include functions and TypeScript will not check their existence or type.
+              Disables type checking and effectively allows all types to be used.
+
+   - unknown: supertype of any other type
+              We may need to describe the type of variables that we do not know when we are writing an application.
+
+   - never:   indicates the values that will never occur. Function throw error or endless loop.
+              BUT can help to catch errors during compile time.
+
+   - boolean: true/false
+   
+   - number:  6   7.10   0xf00d   0b1010   0o744  
+   
+   - string:  or template string
+   
+   - null:    null absence of the object (typeof  = "object" in javascript)
+   
+   - undefined:  undefined means that variable was declared but not initialized
+   
+   - void:  represents the absence of any type, typically used for functions that don’t return a value. 
+            The void type can have undefined as a value where as never cannot have any value.
+   
+   - Array:  typeof  = "object"
+   
+   - Tuple:  A tuple is a typed array with a pre-defined length and types for each index
+
+   - Enum:  is a special "class" that represents a group of constants (unchangeable variables).
+            string 
+            number (by default starts from 0) 
+            Entity which helps you to improve structure of your code.
+   
+   - object:  typeof  = "object"
+
+   - BigInt 
+
+   - Symbol: symbol is a primitive data type, just like number and string. Symbols are immutable, and unique.
+             Just like strings, symbols can be used as keys for object properties.
+
+
+   https://habr.com/ru/sandbox/211287/  [2024]
+
---
+++ test/enum.test.ts
@@ -0,0 +1,95 @@
+test("enum", () => {
+
+	{
+		// ****************************
+		//     1. Enum number
+		//  
+		// will be transformed in map JS to:   
+		//  let Color;  
+		//  (function (Color) {
+		//       Color[Color["Red"] = 0] = "Red";
+		//       Color[Color["Blue"] = 1] = "Blue";
+		//       Color[Color["Green"] = 2] = "Green";
+		//  })(Color || (Color = {}));
+
+		enum Color {
+			Red,    // Default = 0, you can set 2...4.. 56
+			Blue,   // you cna set any index
+			Green
+		}
+
+		expect(Color.Blue).toEqual(1);
+		expect(typeof Color.Blue).toEqual("number");
+		expect(Color[1]).toEqual("Blue");
+		expect(Color["Blue"]).toEqual(1);
+		
+		// We can call enum by index: Reverse enum
+		expect(typeof Color[1]).toEqual("string");
+	}
+
+	{
+		// ****************************
+		//     2. Enum string
+		//  
+		// will be transformed in map JS to: 
+		// string enum does NOT have number index!
+		//   (function (Links) {
+		//       Links["YouTube"] = "www.//yuotube.com";
+		//       Links["Google"] = "www.//google.com";
+		//       Links["Yahoo"] = "www.//yahoo.com";
+		//   })(Links || (Links = {}));
+		enum Links {
+			YouTube = "www.//yuotube.com",    
+			Google = "www.//google.com", 
+			Yahoo = "www.//yahoo.com", 
+		}
+
+		expect(typeof Links.YouTube).toEqual("string");
+		expect(Links.YouTube).toEqual("www.//yuotube.com");
+
+		// !!! We can NOT call string enum by index
+		//log(typeof Links[0]);     // string
+
+	}
+
+	{
+		// ****************************
+		//  3. const Enum  number
+		//  will be NOT transformed in map JS to: 
+		//  Only if this enum will be invoked in code
+		const enum ColorConst{
+			Red,    // Default = 0, you can set 2...4.. 56
+			Blue,   // you cna set any index
+			Green
+		}
+
+		// map JS:  if (0 /* ColorConst.Red */ === 0) {
+		expect(ColorConst.Red).toEqual(0);
+
+		// map JS: if (1 /* ColorConst.Blue */ === 1) {
+		expect(ColorConst.Blue).toEqual(1);
+
+		// !!! can NOT access by index
+		//log(typeof ColorConst[1]);   
+		//if(ColorConst[1]==="Blue") {
+		//	log("Blue");
+		//}
+	}
+
+	{	
+		// ****************************
+		//     4. const Enum  string
+		//   will be NOT transformed in map JS to: 
+		//  Only if this enum will be invoked in code
+		const enum Links2 {
+			YouTube = "www.//yuotube.com",    
+			Google = "www.//google.com", 
+			Yahoo = "www.//yahoo.com", 
+		}
+
+		// map JS :  if ("www.//yuotube.com" /* Links2.YouTube */ === "www.//yuotube.com") {
+		expect(Links2.YouTube).toEqual("www.//yuotube.com");
+
+	}
+
+});
